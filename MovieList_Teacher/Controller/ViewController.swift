d9668fceb330011c28ce07e8e364724aafce7306
IngaKirsona <ingas@inbox.lv>
2020-09-06 17:55:39 +0300

    1:19

--- MovieList_Teacher/Controller/ViewController.swift
+++ MovieList_Teacher/Controller/ViewController.swift
@@ -11,12 +11,13 @@ import UIKit
 class ViewController: UIViewController {
     
     
+    @IBOutlet weak var tableView: UITableView!
     var movies = Movie.createMovie()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     func configureCell(cell:MovieTableViewCell, for indexPath: IndexPath){
         let movie = movies[indexPath.row]
@@ -37,10 +38,32 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate{
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell", for: indexPath) as? MovieTableViewCell else {
             return UITableViewCell()
         }
-        
         configureCell(cell: cell, for: indexPath)
-        
-        
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let alert = UIAlertController(title: "Are You sure you want to delete?", message: nil, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            
+            alert.addAction(UIAlertAction(title: "Delete", style: .default, handler: { (_) in
+                self.movies.remove(at: indexPath.row)
+                UIView.transition(with: tableView, duration: 1.0, options: .transitionCrossDissolve, animations: {
+                    self.tableView.reloadData()
+                }, completion: nil)
+            }))
+            self.present(alert, animated: true)
+        }
+    }
+    
+    
+//
+//    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+//        let currentMovie = movies.remove(at: sourceIndexPath.row)
+//        movies.insert(currentMovie, at: destinationIndexPath.row)
+//    }
+    
+    
+    
 }
